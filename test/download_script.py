fdcb09e332520e0250ab92f14190c25ab48a42bb
tsarpaul <Litvakpol@012.net.il>
2017-02-04 20:19:14 +0200

    Added gif image

---
+++ test/download_script.py
@@ -0,0 +1,6 @@
+"""random script"""
+import os
+
+def download_from_youtube(url):
+	"""random script"""
+	print('started downloading!')	
---
+++ test/utils/print_utils.py
@@ -0,0 +1,4 @@
+
+def print_nicely(prefix, text):
+	print(prefix + '- ' + text)
+
